bf462d851beeccc7c5ee2eff4125207eeb52cbea
hi10-b <41531756+hi10-b@users.noreply.github.com>
2024-10-26 20:02:48 +1100

    Update add_name_img.py

--- whatsapp messaging/photo/add_name_img.py
+++ whatsapp messaging/photo/add_name_img.py
@@ -1,40 +1,59 @@
-from PIL import Image, ImageDraw, ImageFont
 import os
+import csv
+from PIL import Image, ImageDraw, ImageFont
+import pandas as pd
+
+
+
+def get_names_from_excel(contacts_file):
+    with open(contacts_file, newline='') as csvfile:
+        contacts_reader = csv.DictReader(csvfile)
+        for row in contacts_reader:
+            # print(row['First Name'])
+            # print(row['Last Name'])
+            name = row['First Name'] + ' ' + row['Last Name']
+            output_path = './updated/' + row['Phone 1 - Value'] + '.jpeg'
+            add_text_to_image('./template.jpeg', name , output_path )
 
 # Function to add text to image
-def add_text_to_image(image_path, text, output_path):
+def add_text_to_image(image_path, name, output_path):
+
     # Open the image
     image = Image.open(image_path)
-    
+
     # Initialize ImageDraw
     draw = ImageDraw.Draw(image)
     
     # Load a font (you can specify a path to a TTF file, like Arial)
-    font = ImageFont.truetype("arial.ttf", 40)  # Change path if necessary
+    font = ImageFont.truetype("./GlacialIndifference-Bold.otf", 82)  # Cha3nge path if necessary
     
+    _, _, w, h = draw.textbbox((0, 0), name, font=font)
+
+    width = 2047
+    height = 2560
+
     # Define text position and color
-    text_position = (50, 50)  # Adjust as needed
-    text_color = (0, 0, 0)  # Update text colour as required
+    text_position = ((width-w)/2, (height-h)/4)  # Adjust as needed
+    text_color = (255, 255, 255)  # Update text colour as required
+
 
     # Add text to the image
-    draw.text(text_position, text, fill=text_color, font=font)
+    draw.text(text_position, name, fill=text_color, font=font)
     
     # Save the image with the text added
     image.save(output_path)
     print(f"Image saved to {output_path}")
 
-# List of custom texts to add
-texts = ["Alice", "Bob", "Charlie", "David", "Eve"]
-
 # Define the input image path and output folder
-input_image_path = './sample.png'  # Path to the original image
+input_image_path = './template.jpeg'  # Path to the original image
 output_folder = './updated/'  # Folder to save output images
 
 # Create the output folder if it doesn't exist
 os.makedirs(output_folder, exist_ok=True)
 
-# Loop through the list of texts and add them to the image
-for text in texts:
-    # Create the output file path with the text as the filename
-    output_path = os.path.join(output_folder, f"{text}.png")
-    add_text_to_image(input_image_path, text, output_path)
+
+if __name__ == "__main__":
+
+    contacts_file = 'contacts.csv'
+    # contacts_file = 'test.csv'
+    get_names_from_excel(contacts_file)
